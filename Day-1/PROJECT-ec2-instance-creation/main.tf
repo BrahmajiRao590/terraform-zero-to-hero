4685dc9646a11778ee86e9a4b4e68f222095c4e8
BrahmajiRao590 <134035342+BrahmajiRao590@users.noreply.github.com>
2024-01-01 16:15:13 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "ap-south-1"  # Set your desired AWS region
+    region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-03f4878755434977f"  
+    ami           = "ami-0c7217cdde317cfec"  
     instance_type = "t2.micro"
     subnet_id = "subnet-0d03a3c3128dddbec"
     key_name = "Jenkins"
