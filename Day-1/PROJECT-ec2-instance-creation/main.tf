1aaea8bf67011cf73cfdfb81b375d5d1a1ca815b
BrahmajiRao590 <134035342+BrahmajiRao590@users.noreply.github.com>
2024-01-01 13:53:49 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,4 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-03f4878755434977f"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-0d03a3c3128dddbec"
+    key_name = "Jenkins"
 }
