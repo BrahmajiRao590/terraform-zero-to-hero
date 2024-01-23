511cddb53e4ed74029b42fa652895effa5cc0e89
BrahmajiRao590 <134035342+BrahmajiRao590@users.noreply.github.com>
2024-01-23 18:58:14 +0530

    Update variable.tf

--- Day-8/variable.tf
+++ Day-8/variable.tf
@@ -1,9 +1,12 @@
 variable "ami_value" {
 description = "value for the ami"
+}
 
 variable "aws_instance_type" {
 description = "value for the instance type"
 default = "t2.micro"
+}
 
 variable "subnet_id" {
 description = "value for the sunet id"
+}
